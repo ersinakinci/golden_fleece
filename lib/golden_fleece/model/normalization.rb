84475c3715d00248052dbd188471667816c37e2c
Ersin Akinci <ersin.akinci@instacart.com>
2017-05-25 17:09:14 -0700

    Fix normalization

--- lib/golden_fleece/model/normalization.rb
+++ lib/golden_fleece/model/normalization.rb
@@ -6,15 +6,13 @@ module GoldenFleece
       def normalize_fleece
         self.class.fleece_context.schemas.each do |attribute, schema|
           persisted_json = read_attribute attribute
+          computed_json = deep_stringify_keys schema.reduce({}) { |memo, (schema_name, schema)|
+            memo[schema_name] = schema.value.compute(self)
+            memo
+          }
 
-          schema.each do |schema_name, schema|
-            schema_name = schema_name.to_s
-            computed_json = { schema_name => schema.value.compute(self) }
-            deep_stringify_keys computed_json if computed_json.is_a? Hash
-
-            if !persisted_json[schema_name].nil? && persisted_json[schema_name] != computed_json[schema_name]
-              write_attribute attribute, computed_json
-            end
+          if !persisted_json.nil? && persisted_json != computed_json
+            write_attribute attribute, computed_json
           end
         end
       end
